cee16df65b04bd23f021a54d2b902b8335a9f0f1
Dawn Olivier <dmarieolivier@gmail.com>
2022-04-29 17:06:16 -0400

    commit tree 8de0e79b612451fb1825e607bf0d5a9ac916b583

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function() {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (num) {
+    return function (fare) {
+        return num*fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2) // doubles fare //
+
+const fareTripler = createFareMultiplier(3) // triples fare //
+
+const selectDifferentDrivers = (arrayofDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(arrayofDrivers);
+}
